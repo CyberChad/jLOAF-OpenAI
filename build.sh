df6a1d2e682a417f507956f0c30719c573744c72
Chad Peters <chadpeters@hotmail.com>
2017-10-21 12:50:21 -0400

    compiled binaries test
    shell script to compile in linux environment (to remove later)

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -g -verbose -classpath "/home/chad/github/JLOAF/bin" -sourcepath "/home/chad/github/jLOAF-OpenAI/src/" -d "/home/chad/github/jLOAF-OpenAI/bin" ./src/PerformanceTesting/PerformanceTest.java
